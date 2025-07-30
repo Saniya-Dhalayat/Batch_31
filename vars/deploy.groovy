0f34a892df48cd6be82b51d73c405943b5905854
Saniya_Dhalayat <saniyasd16@gmail.com>
2025-07-30 23:37:51 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -15,7 +15,7 @@ def call(String configFile) {
     stage("Run Ansible Playbook") {
         sh """
             cd ${conf.CODE_BASE_PATH}
-            ansible-playbook site.yml -i inventory --extra-vars "env=${conf.ENVIRONMENT}"
+            ansible-playbook env/prod/Assignment5/site.yml -i env/prod/Assignment5/inventory.ini --extra-vars "env=${conf.ENVIRONMENT}"
         """
     }
 
